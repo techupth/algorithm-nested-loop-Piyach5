fb7e7cec6a6f49ff5193101c97443b4aaf2b65b0
Piyachat Jongtong <wearingandtearing5221@gmail.com>
2024-07-03 12:39:25 +0700

    feat: create nested loop func

--- ex-3.js
+++ ex-3.js
@@ -1,4 +1,12 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function multiplicationTable() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+function multiplicationTable(n) {
+  let result = "";
+  for (let i = 1; i <= n; i++) {
+    let row = "";
+    for (let j = 1; j <= n; j++) {
+      row += i * j + "\t";
+    }
+    result += row + "\n";
+  }
+  return result;
 }
